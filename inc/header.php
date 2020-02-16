e4dfd24d8479ba49212698ff58d090104de838b6
Влад <setlefo.gmail.com>
2020-02-16 21:36:47 +0200

    adding my files to my repository

---
+++ inc/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>the</title>
+	<link href="css/normalize.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<header class="main-header">
+		<nav class="main-navigation">
+		<!--
+			<a class="main-header-logo">
+				<img src="img/THE-BIKE-SHOP.png" width="160" height="50">
+			</a>
+		-->
+		<div class="main-navigation-wrapper">
+			<div class="container">
+				<a class="main-header-logo">
+					<img src="img/THE-BIKE-SHOP.png" width="160" height="50">
+				</a>
+				<ul class="site-navigation">
+
+					<li class="sub-menu-parent li-style-sub-menu">
+						<a href="#categories">BICYCLES</a>
+						<ul class="sub-menu">
+							<li class="li-style-a">
+								<a href="#categories">FIXED</a><a href="#categories">SINGLE SPEED</a>
+							</li>
+							<li class="li-style-a">
+								<a href="#categories">CITY BIKES</a>
+							</li>
+							<li class="li-style-a">
+								<a href="#categories">PREMIUM SERIES</a>
+							</li>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">PARTS</a>
+				</li>
+				<li>
+					<a href="#">ACCESSORIES</a>
+				</li>
+				<li>
+					<a href="#">EXTRAS</a>
+				</li>
+				<li>
+					<a href="#">BOX</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="main-header-text">
+	<h1 class="slider-title">HANDMADE BICYCLE</h1>
+
+	<p>You <span class="yellow">create</span> the <span class="yellow">journey,</span>
+		we supply the <span class="yellow">parts.</span></p>
+
+		<button class="slider-button">
+			SHOP BIKES
+		</button>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,132 @@
+<?php require_once 'inc/header.php' ?>
+
+<main class="background-main">
+	<section class="categories">
+
+		<h2 id="categories" class="title-categories">CATEGORIES</h2>
+
+		<ul class="categories-list">
+
+			<div class="categories-tryangle ">
+				<figure class="categories-image image-1">
+					<!-- <a href="#">
+						<img src="img/CATEGORIES_1.png" alt="" width="346px" height="288px"> -->
+						<li class="categorie-item">
+							<h3>FIXED / SINGLE SPEED</h3>
+							<p>Are You ready for the 27.5 Revolution?</p>
+						</li>
+						<!-- </a> -->
+					</figure>
+				</div>
+
+				<div class="categories-tryangle">
+					<figure class="categories-image image-2">
+						<li class="categorie-item">
+							<h3>PREMIUM SERIES</h3>
+							<p>Exclusive  Bike Components</p>
+							<button class="categories-button">
+								GO TO STORE
+							</button>
+						</li>
+					</figure>
+				</div>
+				<div class="categories-tryangle ">
+					<figure class="categories-image image-3">
+						<li class="categorie-item">
+							<h3>CITY BIKES</h3>
+							<p>Street Playground</p>
+						</li>
+					</figure>
+				</div>
+			</ul>
+		</section>
+
+		<section class="gallery">
+			<div class="gallery-container">
+				<h2 class="gallery-title">POPULAR BIKES</h2>
+				<div class="gallery-section">
+					<div class="gallery-section-one">
+						<img src="img/ITEM_PHOTO1.png" alt="" width="470" height="370">
+						<div class="gallery-section-main">
+							<p>FIXED GEAR<br>
+								<span class="none">$249.00</span></p>
+								<select name="OPTION" id="" class="gallery-select">
+									<option value="1">OPTION</option>
+									<option value="2"></option>
+								</select>
+								<button class="gallery-button">BUY</button>
+							</div>
+						</div>
+						<div class="gallery-section-second">
+							<img src="img/ITEM_PHOTO_2.png" alt="" width="470" height="370">
+							<div class="gallery-section-main">
+								<p>BIG BOY ULTRA<br>
+									<span class="none">$249.00</span></p>
+									<select name="OPTION" id="" class="gallery-select">
+										<option value="1">OPTION</option>
+										<option value="2"></option>
+									</select>
+									<button class="gallery-button">BUY</button>
+								</div>
+							</div>
+							<div class="gallery-section-third">
+								<img src="img/ITEM_PHOTO_3.png" alt="" width="470" height="370">
+								<div class="gallery-section-main">
+									<p>SANCHAEZ<br>
+										<span class="none">$249.00</span></p>
+										<select name="OPTION" id="" class="gallery-select">
+											<option value="1">OPTION</option>
+											<option value="2"></option>
+										</select>
+										<button class="gallery-button">BUY</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+
+					<section class="contact">
+						<h2 class="contact-title">CONTACT US</h2>
+						<p class="contact-text contact-form">Please contact us for all inquiries and purchase options.</p>
+						<div class="contact-container ">
+							<form action="" class="contact-form">
+								<div class="username-surname">
+									<!-- <label for="username"></label> -->
+									<input type="text" id="username" placeholder="NAME" class="username">
+									<!-- <label for="surname"></label> -->
+									<input type="text" id="surname" placeholder="SURNAME" class="surname">
+									<!-- <label for="email"></label> -->
+								</div>
+								<input type="email" id="email" placeholder="USER@DOMAIN.COM" class="email">
+							</form>
+							<textarea name="message" id="" placeholder="MESSAGE"></textarea>
+							<button class="contact-button contact-form">SEND</button>
+						</div>
+					</section>
+				</main>
+				<footer class="main-footer">
+					<nav class="main-footer-navigation">
+						<div class="footer-container">
+							<a class="main-footer-logo">
+								<img src="img/THE_BIKE_SHOP_LOGO_DARK.png" width="115" height="36">
+							</a>
+							<ul class="footer-navigation">
+								<li>
+									<a href="#">BICYCLES</a>
+								</li>
+								<li>
+									<a href="#">PARTS</a>
+								</li>
+								<li>
+									<a href="#">ACCESSORIES</a>
+								</li>
+								<li>
+									<a href="#">EXTRAS</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</footer>
+
+			</body>
+			</html>
